85f2ec6ecfa4ff13cf4f676421f5669d5e046d94
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-03 19:08:09 +0100

    Refactor: add google sheets code to new module google_sheets.py
    add try catch block for any errors that may arrise

---
+++ google_sheets.py
@@ -0,0 +1,31 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+# sets what im authorized to use with google cloud services
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+
+try:
+    # get the credentials from the config var
+    CREDS = Credentials.from_service_account_file("creds.json")
+    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+    # open the google sheets for budget trainer
+    SHEET = GSPREAD_CLIENT.open("budget-trainer")
+
+    # get the income sheet
+    INCOME = SHEET.worksheet("income")
+    # gets the expenses sheet
+    EXPENSES = SHEET.worksheet("expenses")
+
+except FileNotFoundError:
+    print("Error: The credentials file 'creds.json' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+
