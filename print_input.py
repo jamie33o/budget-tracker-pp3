51604e9e72e1421f8cf778df4ddd7dddb43089fe
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-04 17:41:37 +0100

    Update: update input validator function to validate username

--- print_input.py
+++ print_input.py
@@ -1,5 +1,6 @@
 import sys  # System-specific parameters and functions
 import time  # provides functions for handling time-related tasks
+import os
 
 
 
@@ -13,20 +14,38 @@ def slow_print_effect(text, delay=0.04):
         time.sleep(delay)
 
 
-def input_validator(expected_type):
+def input_validator(expected_type, input_text):
+    """checks if input valid depending on expected type if its 
+    string or int and returns the input if there is no error"""
     input_value = ""
-    if expected_type == "str":
+    if expected_type == "letter":
         while True:
-                input_value = input("\nType Y for yes and N for No (Y/N): \n")
+                input_value = input(f"\n {input_text}\n")
                 if input_value.upper() in ["Y", "N"]:
+                       clear_terminal()
                        break  # Exit the loop if the input is a valid string
                 else: 
                     slow_print_effect("Wrong input! Please enter text.")
-    else:
+    elif expected_type == "number":
         while True:
                 try:
-                    input_value = int(input("\nEnter the cost as a whole number: \n"))
+                    input_value = int(input(f"\n{input_text}\n"))
                     break  # Exit the loop if the input is a valid integer
                 except ValueError:
                     slow_print_effect("Wrong input! Please enter a whole number.")
+    elif expected_type == "word": 
+        while True:
+            input_value = input(f"\n {input_text}\n")
+            if len(input_value) in range(5,11):
+                    clear_terminal()
+                    break  # Exit the loop if the input is a valid string
+            else: 
+                slow_print_effect("Wrong input! Please enter text.")
     return input_value
+
+
+def clear_terminal():
+    """
+    function to clear terminal
+    """
+    os.system('cls' if os.name == 'nt' else 'clear') 
