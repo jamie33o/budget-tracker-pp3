d1ec3de605ef2124519712604fda9b86ac21374e
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-07 20:48:25 +0100

    Update: main function to get username from the user and call menu and pass username
    also update the the logo as there was an error in how it displayed

--- run.py
+++ run.py
@@ -1,31 +1,31 @@
 from print_input import slow_print_effect
-from google_sheets import register
+from menu import menu
+from print_input import input_validator
 
 
-LOGO = """
-                         /$$$$$$$                  /$$                       /$$           /$$$$$$$$                 /$$
-                        | $$__  $$                | $$                      | $$          |__  $$__/                |__/
-                        | $$  \ $$ /$$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$           | $$  /$$$$$$  /$$$$$$  /$$ /$$$$$$$   /$$$$$$   /$$$$$$
-                        | $$$$$$$ | $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/           | $$ /$$__  $$|____  $$| $$| $$__  $$ /$$__  $$ /$$__  $$
-                        | $$__  $$| $$  | $$| $$  | $$| $$  \ $$| $$$$$$$$  | $$             | $$| $$  \__/ /$$$$$$$| $$| $$  \ $$| $$$$$$$$| $$  \__/
-                        | $$  \ $$| $$  | $$| $$  | $$| $$  | $$| $$_____/  | $$ /$$         | $$| $$      /$$__  $$| $$| $$  | $$| $$_____/| $$     
-                        | $$$$$$$/|  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$  |  $$$$/         | $$| $$     |  $$$$$$$| $$| $$  | $$|  $$$$$$$| $$     
-                        |_______/  \______/  \_______/ \____  $$ \_______/   \___/           |__/|__/      \_______/|__/|__/  |__/ \_______/|__/     
-                                                       /$$  \ $$  
-                                                      |  $$$$$$/   
-                                                       \______/    
-"""
-
-
+LOGO = ["""  /$$$$$$$                  /$$                       /$$           /$$$$$$$$                           /$$                          
+            | $$__  $$                | $$                      | $$          |__  $$__/                          | $$                          
+            | $$  \ $$ /$$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$           | $$  /$$$$$$  /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$   /$$$$$$ 
+            | $$$$$$$ | $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/           | $$ /$$__  $$|____  $$ /$$_____/| $$  /$$/ /$$__  $$ /$$__  $$
+            | $$__  $$| $$  | $$| $$  | $$| $$  \ $$| $$$$$$$$  | $$             | $$| $$  \__/ /$$$$$$$| $$      | $$$$$$/ | $$$$$$$$| $$  \__/
+            | $$  \ $$| $$  | $$| $$  | $$| $$  | $$| $$_____/  | $$ /$$         | $$| $$      /$$__  $$| $$      | $$_  $$ | $$_____/| $$      
+            | $$$$$$$/|  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$  |  $$$$/         | $$| $$     |  $$$$$$$|  $$$$$$$| $$ \  $$|  $$$$$$$| $$      
+            |_______/  \______/  \_______/ \____  $$ \_______/   \___/           |__/|__/      \_______/ \_______/|__/  \__/ \_______/|__/      
+                                           /$$  \ $$                                                                                            
+                                           |  $$$$$$/                                                                                            
+                                            \______/
+        """]
 
 
 def main():
     """
     Print logo.
     """
-    slow_print_effect(f"\033[34m{LOGO}\033[0m",0.005)# ANSI escape codes to change the text color
-    slow_print_effect("Welcome to Budget Trainer in this game you will train your memory to remember your budget and keep track of it\n")
-    register()
+    #slow_print_effect(f"\033[34m{LOGO}\033[0m",0.005)# ANSI escape codes to change the text color
+    #slow_print_effect("\nWelcome to Budget Trainer in this game you will train your memory to remember your budget and keep track of it\n")
+    USERNAME = input_validator("username", "Please enter username,\n if you have used this website before please use the same username...\nIt must be 5 to 10 characters long")
+    menu(USERNAME)
+
 
 if __name__ == '__main__':
     main()
