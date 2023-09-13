f46e04751aa09c223c0e46c60c239f8bdf3303bc
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-13 01:14:27 +0100

    Fix: use pyfiglet for logo as old logo was not aligned with pep8 styling
    Also changed menu to return username so when it is called again username will be passed as parameter so user does not have to log in each time

--- run.py
+++ run.py
@@ -1,39 +1,24 @@
+import pyfiglet
 from print_input import slow_print_effect,text_style
 from menu import menu
 
 
-LOGO = """
- 
- /$$$$$$$                  /$$                       /$$                     
-| $$__  $$                | $$                      | $$                     
-| $$  \ $$ /$$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$                   
-| $$$$$$$ | $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/                   
-| $$__  $$| $$  | $$| $$  | $$| $$  \ $$| $$$$$$$$  | $$                     
-| $$  \ $$| $$  | $$| $$  | $$| $$  | $$| $$_____/  | $$ /$$                 
-| $$$$$$$/|  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$  |  $$$$/                 
-|_______/  \______/  \_______/ \____  $$ \_______/   \___/                   
-                               /$$  \ $$                                     
-            /$$$$$$$$         |  $$$$$$/        /$$                          
-           |__  $$__/          \______/        | $$                          
-              | $$  /$$$$$$  /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$   /$$$$$$ 
-              | $$ /$$__  $$|____  $$ /$$_____/| $$  /$$/ /$$__  $$ /$$__  $$
-              | $$| $$  \__/ /$$$$$$$| $$      | $$$$$$/ | $$$$$$$$| $$  \__/
-              | $$| $$      /$$__  $$| $$      | $$_  $$ | $$_____/| $$      
-              | $$| $$     |  $$$$$$$|  $$$$$$$| $$ \  $$|  $$$$$$$| $$      
-              |__/|__/      \_______/ \_______/|__/  \__/ \_______/|__/                                            
-"""
 
 
 def main():
     """
     Prints logo and welcome message then calls menu
     """
-    slow_print_effect(text_style("success",LOGO),0.005)# ANSI escape codes to change the text color
+
+    logo = pyfiglet.figlet_format("Budget \n    Tracker", font = "digital")
+
+    slow_print_effect(text_style("success",logo),0.005)# ANSI escape codes to change the text color
     slow_print_effect(text_style("info",
                                  """\nWelcome to Budget Tracker
                                  \nBudget Tracker helps you keep track of your budget\n"""))
+    username = None
     while True:
-        menu()
+        username = menu(username)
 
 #  checks if the Python script is being run as the main program and not imported as a module
 if __name__ == '__main__':
