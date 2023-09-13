9d36bf5a50dc737c6c1b19d815d5e816ff554e50
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-13 21:01:46 +0100

    Refactor: align code with pep8 and code institute linter styling guidelines

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import pyfiglet
-from print_input import slow_print_effect,text_style
+from print_input import slow_print_effect
 from menu import menu
-
-
 
 
 def main():
@@ -10,16 +8,20 @@ def main():
     Prints logo and welcome message then calls menu
     """
 
-    logo = pyfiglet.figlet_format("Budget \n    Tracker", font = "digital")
-
-    slow_print_effect(text_style("success",logo),0.005)# ANSI escape codes to change the text color
-    slow_print_effect(text_style("info",
-                                 """\nWelcome to Budget Tracker
-                                 \nBudget Tracker helps you keep track of your budget\n"""))
+    logo = pyfiglet.figlet_format("Budget \n    Tracker", font="digital")
+    # ANSI escape codes to change the text color
+    slow_print_effect("success", logo, 0.05)
+    slow_print_effect("info",
+                      "Welcome to Budget Tracker,"
+                      "\nYour personal budget management tool.\n"
+                      "Budget Tracker is here to help you keep "
+                      "a close eye on your finances.\n")
     username = None
     while True:
         username = menu(username)
 
-#  checks if the Python script is being run as the main program and not imported as a module
+
+#  checks if the Python script is being run
+# as the main program and not imported as a module
 if __name__ == '__main__':
     main()
