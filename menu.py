b4460fa825e077af0cdcaa4712ff6ade2c32db57
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-13 01:09:54 +0100

    Refactor: move code in menu options to seperate functions
    Change name of prices dictionary to expenses dict and also align code with pep8 styling

--- menu.py
+++ menu.py
@@ -1,19 +1,27 @@
 from tabulate import tabulate  # table prints
-from print_input import * # import all functions from print_input
+from print_input import text_style, input_validator, slow_print_effect,clear_terminal
 from questions import questions
-from google_sheets import * # import all google_sheets functions
+from google_sheets import (
+    delete_user,
+    get_budget,
+    register, login,
+    budget_overview,
+    change_budget,
+    change_username,
+    add_expenses
+)
 
 
 
-def menu():
+def menu(username):
     """main menu """
     input(text_style("input","Press Enter to view the menu\n"))
 
 
     clear_terminal()
 
-    prices = {# dictionary matching google sheets expenses work sheet
-        "username": "",
+    expenses_dict = {# dictionary matching google sheets expenses work sheet
+        "username": username,
         "date": "",
         "work-commute": 0,
         "work-lunch": 0,
@@ -28,8 +36,8 @@ def menu():
     }
 
     if not username: # check if user already logged in, otherwise show this menu
-        username = login_register(prices)
-        prices["username"] = username
+        username = login_or_register(expenses_dict,username)
+        expenses_dict["username"] = username
 
     clear_terminal()
 
@@ -43,7 +51,7 @@ def menu():
         ["6", "View budget overview for previos 7 days from date entered"],
         ["7", "Delete all data linked to username"],
     ]
-    keys_list = list(prices.keys()) # keys list of prices menu
+    keys_list = list(expenses_dict.keys()) # keys list of prices menu
 
     print(tabulate(menu_options))
 
@@ -71,66 +79,18 @@ def menu():
         if budget_changed:
             slow_print_effect(text_style("success","Budget Updated!!!!\n"))
     elif choice == 3:
-        # Calls questions function so user can add expenses then it adds 
-        # the expenses entered to google sheets expenses worksheet
-        prices_dict = questions(None,prices)
-        expenses_added = add_expenses(prices_dict)
-        if expenses_added:
-            slow_print_effect(text_style("success","Expenses Added!!!!\n"))
+        update_user_expenses(expenses_dict,False)
     elif choice == 4:
-        # User enters a date then adds expenses and then 
-        # expenses worksheet is updated for the date entered
-        date = input_validator("date", text_style("input",
-                                    "Please enter the date you want to update format YYYY-MM-DD"))
-        prices_dict = questions(date,prices)
-        expenses_added = add_expenses(prices_dict)
-        if expenses_added:
-            slow_print_effect(text_style("success","Expenses Updated!!!!\n"))
+        update_user_expenses(expenses_dict,True)
     elif choice == 5:
-        #budget overview is retrieved from google sheets 
-        # then the expenses are shown in the terminal in a table
-        results = budget_overview(username,None)
-        if results :
-            tabulate_data(results,keys_list,username)
-        else:
-            slow_print_effect(text_style("error","Sorry you have no expenses added!!!\n"))
-
+        view_budget_overview_by_date(username,keys_list,False)
     elif choice == 6:
-        # User enters a date and the expenses are retrieved for 7days before that date
-        date = input_validator("date",
-                               text_style("input",
-                                    """Please enter the date for the budget, 
-                                       which will be 7 days previous to the date you provide.
-                                       \nPlease enter date as YYYY-MM-DD"""))
-        results = budget_overview(username,date)
-        if results:
-            tabulate_data(results,keys_list,username)
-        else:
-            slow_print_effect(text_style("error","Sorry we have no expenses before that date!!!\n"))
-
+        view_budget_overview_by_date(username,keys_list,True)
     elif choice == 7:
-        # User enters there username and if it matches the current
-        # username they are logged in with there account is deleted
-        username_to_delete = input_validator("username",text_style("input",
-                                                                   "Please enter your username"))
-        if username == username_to_delete:
-            ans = input_validator("letter",
-                                  text_style("input",
-                                             f"""Are you sure you want to delete all data belonging to user 
-                                             {username_to_delete}
-                                             ?\n Type Y for YES and N for NO (Y/N)"""))
-            if ans == "Y":
-                user_deleted = delete_user(username_to_delete)
-                if user_deleted:
-                    slow_print_effect(text_style("success","Account deleted!!!\n"))
-                    username = None
-                else:
-                    slow_print_effect(text_style("error","Account not deleted!!!\n"))
-
-        else:
-            slow_print_effect(text_style("error","Wrong Username!!!\n"))
+        username = delete_account(username)
     else:
         slow_print_effect(text_style("error",f"{choice} is not an option!!!\n"))
+    return username
 
 
 
@@ -197,7 +157,7 @@ def tabulate_data(results,keys_list, username):
         print(tabulate([total_expenses],["Budget","Total spent","Saved"],"fancy_outline"))
 
 
-def login_register(prices):
+def login_or_register(expenses_dict,username):
     """shows a menu for the user to log in or register
 
     Parameters:
@@ -215,11 +175,10 @@ def login_register(prices):
         choice = input_validator("number", text_style("input","Please choose an option"))
         if choice in [1,2]:
             break
-        else:
-            print(text_style("error","Please enter 1 or 2\n"))
+        print(text_style("error","Please enter 1 or 2\n"))
     while True:
         # user enter username for login or register
-        username = input_validator("username", 
+        username = input_validator("username",
                                     text_style("input",
                                             """Please enter a username
                                             \nIt must be 5 to 10 characters long"""))
@@ -229,29 +188,106 @@ def login_register(prices):
             if logged_in:
                 slow_print_effect(text_style("success","Congradultion you are logged in!!!\n"))
                 return username
-            else:
-                print(text_style("error",
-                                    f"""{username} does not exist... 
-                                    \nType 2 to register\nType 1 to try again\n"""))
-                # if username is not in the income worksheet the
-                #  user can type 1 to try again or type 1 to register
-                new_choice = input_validator("number",
-                                                text_style("input","Please choose an option"))
-                if new_choice == 2:
-                    choice = new_choice
-                username = None
+
+            print(text_style("error",
+                                f"""{username} does not exist... 
+                                \nType 2 to register\nType 1 to try again\n"""))
+            # if username is not in the income worksheet the
+            #  user can type 1 to try again or type 1 to register
+            new_choice = input_validator("number",
+                                            text_style("input","Please choose an option"))
+            if new_choice == 2:
+                choice = new_choice
+            username = None
         else:
             # if  the user chose register there username will be added to income worksheet
             registered = register(username)
             if registered:
                 slow_print_effect(text_style("success","Congradultion you are registered!!!\n"))
                 # the user will be then asked to add expenses
-                prices["username"] = username
-                prices_dict = questions(None,prices)
-                expenses_added = add_expenses(prices_dict)
+                expenses_dict["username"] = username
+                updated_expenses_dict = questions(None,expenses_dict)
+                expenses_added = add_expenses(updated_expenses_dict)
                 if expenses_added:
                     slow_print_effect(text_style("success","Expenses Added!!!!\n"))
                 return username
-            else:
-                print(text_style("error","Error registering... Please try again...\n"))
-                username = None
+            print(text_style("error","Error registering... Please try again...\n"))
+            return None
+
+
+def update_user_expenses(prices,update_by_date_bool):
+    """
+    if update_by_date_bool is true user is asked to enter a 
+    date then the user is asked to add expenses and then 
+    expenses worksheet is updated for the date entered 
+    if update_by_date_bool is set to false expense are updated for current date
+
+    Parameters:
+        prices(dictionary): keys match the googlesheets expenses worksheet fields 
+        and the value will be the expenses the user adds
+
+        update_by_date_bool(boolean): for setting if user can enter a date or use current date
+    """
+
+    if update_by_date_bool:
+        date = input_validator("date", text_style("input",
+                                    "Please enter the date you want to update format YYYY-MM-DD"))
+        prices_dict = questions(date,prices)
+    else:
+        prices_dict = questions(None,prices)
+    expenses_added = add_expenses(prices_dict)
+    if expenses_added:
+        slow_print_effect(text_style("success","Expenses Updated!!!!\n"))
+
+
+def view_budget_overview_by_date(username,keys_list,search_by_date_bool):
+    """if search_by_date_bool is true user is asked to enter a date 
+    and the expenses are retrieved for 7days before that date
+    if it is false expenses are retrieved from the current date
+
+    Parameters:
+        username (username): username user enters on login or register
+        keys_list(list): list of keys from the prices dictionary
+        search_by_date_bool(boolean): boolean if true user can get overview 
+                                        by date if false search from current date
+       
+    """
+    if search_by_date_bool:
+        date = input_validator("date",
+                                text_style("input",
+"""Please enter the date for the budget, which will be 7 days previous to the date you provide.
+\nPlease enter date as YYYY-MM-DD"""))
+
+        results = budget_overview(username,date)
+    else:
+        results = budget_overview(username,None)
+    if results:
+        tabulate_data(results,keys_list,username)
+    else:
+        slow_print_effect(text_style("error","Sorry we have no expenses before that date!!!\n"))
+
+
+def delete_account(username):
+    """
+        User enters there username and if it matches the current
+        username they are logged in with there account is deleted
+    """
+    username_to_delete = input_validator("username",text_style("input",
+                                                                "Please enter your username"))
+    if username == username_to_delete:
+        ans = input_validator("letter",
+                                text_style("input",
+                                                "Are you sure you want to delete all data "
+                                                f"belonging to use {username_to_delete}?"
+                                                "\n Type Y for YES and N for NO (Y/N)"))
+        if ans == "Y":
+            user_deleted = delete_user(username_to_delete)
+            if user_deleted:
+                slow_print_effect(text_style("success","Account deleted!!!\n"))
+                return None
+
+            slow_print_effect(text_style("error","Account not deleted!!!\n"))
+        return username
+
+    slow_print_effect(text_style("error","Wrong Username!!!\n"))
+    return username
